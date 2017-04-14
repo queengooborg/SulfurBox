4f30d27485b7c43cb937182b4c2bbe308ad5509c
brandon3055 <brandon4261@gmail.com>
2017-04-14 17:42:21 +1000

    Added example code to launch gui in main method

--- src/vdscratchy/sulfurbox/Main.java
+++ src/vdscratchy/sulfurbox/Main.java
@@ -1,5 +1,9 @@
 package vdscratchy.sulfurbox;
 
+import vdscratchy.sulfurbox.gui.SulfurBoxGui;
+
+import javax.swing.*;
+
 /**
  * SulfurBox - ${PACKAGE_NAME}
  * Created by vinyldarkscratch on 4/13/17.
@@ -7,5 +11,16 @@ package vdscratchy.sulfurbox;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Opening the Box!!!");
+        
+        UIManager.installLookAndFeel("Darcula", "com.bulenkov.darcula.DarculaLaf");
+
+        //Ensures the UI opens in its own thread and does not interfere with the main thread.
+        //I dont think this is required because swing already creates a separate event scheduler thread but i seen someone else do this so im doing it!
+        SwingUtilities.invokeLater(() -> {
+            SulfurBoxGui gui = new SulfurBoxGui();
+            gui.setVisible(true);
+        });
+        
     }
 }
