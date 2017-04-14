dd0edd9f615a8fb2261672dfb8aebe5b12563ec8
Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2017-04-14 02:21:03 -0700

    Add Darcula theme lib and adjust Main class

--- src/vdscratchy/sulfurbox/Main.java
+++ src/vdscratchy/sulfurbox/Main.java
@@ -5,7 +5,7 @@ import vdscratchy.sulfurbox.gui.SulfurBoxGui;
 import javax.swing.*;
 
 /**
- * SulfurBox - ${PACKAGE_NAME}
+ * SulfurBox - vdscratchy.sulfurbox.Main
  * Created by vinyldarkscratch on 4/13/17.
  */
 public class Main {
@@ -18,8 +18,8 @@ public class Main {
         //Ensures the UI opens in its own thread and does not interfere with the main thread.
         //I dont think this is required because swing already creates a separate event scheduler thread but i seen someone else do this so im doing it!
         SwingUtilities.invokeLater(() -> {
-            SulfurBoxGui gui = new SulfurBoxGui();
-            gui.setVisible(true);
+            val gui = new SulfurBoxGui();
+            gui.visible = true;
         });
         
     }
